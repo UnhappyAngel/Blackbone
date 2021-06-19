5681a3478789a216d1073ced2850fbe017a4055f
unhappyangel <kartridder@gmail.com>
2021-06-19 15:37:34 +0300

    win11 dev update

--- src/3rd_party/VersionApi.h
+++ src/3rd_party/VersionApi.h
@@ -16,7 +16,7 @@
 #define _WIN32_WINNT_WINBLUE        0x0603
 #define _WIN32_WINNT_WIN10          0x0A00        
 
-using fnRtlGetVersion = NTSTATUS( NTAPI* )(PRTL_OSVERSIONINFOEXW lpVersionInformation);
+using fnRtlGetVersion = NTSTATUS(NTAPI*)(PRTL_OSVERSIONINFOEXW lpVersionInformation);
 
 enum eBuildThreshold
 {
@@ -29,6 +29,7 @@ enum eBuildThreshold
     Build_19H1 = 18362,
     Build_19H2 = 18363,
     Build_20H1 = 19041,
+    Build_Win11Beta = 21996,
     Build_RS_MAX = 99999,
 };
 
@@ -48,6 +49,7 @@ enum eVerShort
     Win10_19H1,     // Windows 10 May 2019 update
     Win10_19H2,     // Windows 10 November 2019 update
     Win10_20H1,     // Windows 10 April 2020 update
+    Win11_Beta,
 };
 
 struct WinVersion
@@ -67,26 +69,26 @@ BLACKBONE_API inline uint32_t GetRevision()
 {
     HKEY hKey = NULL;
 
-    if (RegOpenKeyExW( HKEY_LOCAL_MACHINE, L"SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion", 0, KEY_QUERY_VALUE, &hKey ) == 0)
+    if (RegOpenKeyExW(HKEY_LOCAL_MACHINE, L"SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion", 0, KEY_QUERY_VALUE, &hKey) == 0)
     {
         wchar_t data[MAX_PATH] = {};
-        DWORD dataSize = sizeof( data );
+        DWORD dataSize = sizeof(data);
         DWORD type = REG_SZ;
 
-        if (RegQueryValueExW( hKey, L"BuildLabEx", nullptr, &type, reinterpret_cast<LPBYTE>(data), &dataSize ) == 0)
+        if (RegQueryValueExW(hKey, L"BuildLabEx", nullptr, &type, reinterpret_cast<LPBYTE>(data), &dataSize) == 0)
         {
             std::wstring buildStr = data;
-            size_t first = buildStr.find( L'.' );
-            size_t second = buildStr.find( L'.', first + 1 );
+            size_t first = buildStr.find(L'.');
+            size_t second = buildStr.find(L'.', first + 1);
 
             if (second > first && first != buildStr.npos)
             {
-                RegCloseKey( hKey );
-                return std::wcstol( buildStr.substr( first + 1, second - first - 1 ).c_str(), nullptr, 10 );
+                RegCloseKey(hKey);
+                return std::wcstol(buildStr.substr(first + 1, second - first - 1).c_str(), nullptr, 10);
             }
         }
 
-        RegCloseKey( hKey );
+        RegCloseKey(hKey);
     }
 
     return 0;
@@ -95,12 +97,12 @@ BLACKBONE_API inline uint32_t GetRevision()
 BLACKBONE_API inline void InitVersion()
 {
     auto& g_WinVer = WinVer();
-    g_WinVer.native.dwOSVersionInfoSize = sizeof( g_WinVer.native );
+    g_WinVer.native.dwOSVersionInfoSize = sizeof(g_WinVer.native);
 
-    auto RtlGetVersion = (fnRtlGetVersion)GetProcAddress( GetModuleHandleW( L"ntdll.dll" ), "RtlGetVersion" );
+    auto RtlGetVersion = (fnRtlGetVersion)GetProcAddress(GetModuleHandleW(L"ntdll.dll"), "RtlGetVersion");
     if (RtlGetVersion)
     {
-        RtlGetVersion( &g_WinVer.native );
+        RtlGetVersion(&g_WinVer.native);
     }
 
     if (g_WinVer.native.dwMajorVersion != 0)
@@ -109,6 +111,8 @@ BLACKBONE_API inline void InitVersion()
         switch (fullver)
         {
         case _WIN32_WINNT_WIN10:
+            if (g_WinVer.native.dwBuildNumber >= Build_Win11Beta)
+                g_WinVer.ver = Win11_Beta;
             if (g_WinVer.native.dwBuildNumber >= Build_20H1)
                 g_WinVer.ver = Win10_20H1;
             else if (g_WinVer.native.dwBuildNumber >= Build_19H2)
@@ -154,7 +158,7 @@ BLACKBONE_API inline void InitVersion()
 }
 
 VERSIONHELPERAPI
-IsWindowsVersionOrGreater( WORD wMajorVersion, WORD wMinorVersion, WORD wServicePackMajor, DWORD dwBuild )
+IsWindowsVersionOrGreater(WORD wMajorVersion, WORD wMinorVersion, WORD wServicePackMajor, DWORD dwBuild)
 {
     auto& g_WinVer = WinVer();
     if (g_WinVer.native.dwMajorVersion != 0)
@@ -169,7 +173,7 @@ IsWindowsVersionOrGreater( WORD wMajorVersion, WORD wMinorVersion, WORD wService
         else if (g_WinVer.native.dwMinorVersion < wMinorVersion)
             return false;
 
-		if (g_WinVer.native.wServicePackMajor > wServicePackMajor)
+        if (g_WinVer.native.wServicePackMajor > wServicePackMajor)
             return true;
         else if (g_WinVer.native.wServicePackMajor < wServicePackMajor)
             return false;
@@ -185,130 +189,136 @@ BLACKBONE_API
 VERSIONHELPERAPI
 IsWindowsXPOrGreater()
 {
-    return IsWindowsVersionOrGreater( HIBYTE( _WIN32_WINNT_WINXP ), LOBYTE( _WIN32_WINNT_WINXP ), 0, 0 );
+    return IsWindowsVersionOrGreater(HIBYTE(_WIN32_WINNT_WINXP), LOBYTE(_WIN32_WINNT_WINXP), 0, 0);
 }
 
 
 VERSIONHELPERAPI
 IsWindowsXPSP1OrGreater()
 {
-    return IsWindowsVersionOrGreater( HIBYTE( _WIN32_WINNT_WINXP ), LOBYTE( _WIN32_WINNT_WINXP ), 1, 0 );
+    return IsWindowsVersionOrGreater(HIBYTE(_WIN32_WINNT_WINXP), LOBYTE(_WIN32_WINNT_WINXP), 1, 0);
 }
 
 VERSIONHELPERAPI
 IsWindowsXPSP2OrGreater()
 {
-    return IsWindowsVersionOrGreater( HIBYTE( _WIN32_WINNT_WINXP ), LOBYTE( _WIN32_WINNT_WINXP ), 2, 0 );
+    return IsWindowsVersionOrGreater(HIBYTE(_WIN32_WINNT_WINXP), LOBYTE(_WIN32_WINNT_WINXP), 2, 0);
 }
 
 VERSIONHELPERAPI
 IsWindowsXPSP3OrGreater()
 {
-    return IsWindowsVersionOrGreater( HIBYTE( _WIN32_WINNT_WINXP ), LOBYTE( _WIN32_WINNT_WINXP ), 3, 0 );
+    return IsWindowsVersionOrGreater(HIBYTE(_WIN32_WINNT_WINXP), LOBYTE(_WIN32_WINNT_WINXP), 3, 0);
 }
 
 VERSIONHELPERAPI
 IsWindowsVistaOrGreater()
 {
-    return IsWindowsVersionOrGreater( HIBYTE( _WIN32_WINNT_VISTA ), LOBYTE( _WIN32_WINNT_VISTA ), 0, 0 );
+    return IsWindowsVersionOrGreater(HIBYTE(_WIN32_WINNT_VISTA), LOBYTE(_WIN32_WINNT_VISTA), 0, 0);
 }
 
 VERSIONHELPERAPI
 IsWindowsVistaSP1OrGreater()
 {
-    return IsWindowsVersionOrGreater( HIBYTE( _WIN32_WINNT_VISTA ), LOBYTE( _WIN32_WINNT_VISTA ), 1, 0 );
+    return IsWindowsVersionOrGreater(HIBYTE(_WIN32_WINNT_VISTA), LOBYTE(_WIN32_WINNT_VISTA), 1, 0);
 }
 
 VERSIONHELPERAPI
 IsWindowsVistaSP2OrGreater()
 {
-    return IsWindowsVersionOrGreater( HIBYTE( _WIN32_WINNT_VISTA ), LOBYTE( _WIN32_WINNT_VISTA ), 2, 0 );
+    return IsWindowsVersionOrGreater(HIBYTE(_WIN32_WINNT_VISTA), LOBYTE(_WIN32_WINNT_VISTA), 2, 0);
 }
 
 VERSIONHELPERAPI
 IsWindows7OrGreater()
 {
-    return IsWindowsVersionOrGreater( HIBYTE( _WIN32_WINNT_WIN7 ), LOBYTE( _WIN32_WINNT_WIN7 ), 0, 0 );
+    return IsWindowsVersionOrGreater(HIBYTE(_WIN32_WINNT_WIN7), LOBYTE(_WIN32_WINNT_WIN7), 0, 0);
 }
 
 VERSIONHELPERAPI
 IsWindows7SP1OrGreater()
 {
-    return IsWindowsVersionOrGreater( HIBYTE( _WIN32_WINNT_WIN7 ), LOBYTE( _WIN32_WINNT_WIN7 ), 1, 0 );
+    return IsWindowsVersionOrGreater(HIBYTE(_WIN32_WINNT_WIN7), LOBYTE(_WIN32_WINNT_WIN7), 1, 0);
 }
 
 VERSIONHELPERAPI
 IsWindows8OrGreater()
 {
-    return IsWindowsVersionOrGreater( HIBYTE( _WIN32_WINNT_WIN8 ), LOBYTE( _WIN32_WINNT_WIN8 ), 0, 0 );
+    return IsWindowsVersionOrGreater(HIBYTE(_WIN32_WINNT_WIN8), LOBYTE(_WIN32_WINNT_WIN8), 0, 0);
 }
 
 VERSIONHELPERAPI
 IsWindows8Point1OrGreater()
 {
-    return IsWindowsVersionOrGreater( HIBYTE( _WIN32_WINNT_WINBLUE ), LOBYTE( _WIN32_WINNT_WINBLUE ), 0, 0 );
+    return IsWindowsVersionOrGreater(HIBYTE(_WIN32_WINNT_WINBLUE), LOBYTE(_WIN32_WINNT_WINBLUE), 0, 0);
 }
 
 VERSIONHELPERAPI
 IsWindows10OrGreater()
 {
-    return IsWindowsVersionOrGreater( HIBYTE( _WIN32_WINNT_WIN10 ), LOBYTE( _WIN32_WINNT_WIN10 ), 0, 0 );
+    return IsWindowsVersionOrGreater(HIBYTE(_WIN32_WINNT_WIN10), LOBYTE(_WIN32_WINNT_WIN10), 0, 0);
 }
 
 VERSIONHELPERAPI
 IsWindows10RS1OrGreater()
 {
-    return IsWindowsVersionOrGreater( HIBYTE( _WIN32_WINNT_WIN10 ), LOBYTE( _WIN32_WINNT_WIN10 ), 0, Build_RS1 );
+    return IsWindowsVersionOrGreater(HIBYTE(_WIN32_WINNT_WIN10), LOBYTE(_WIN32_WINNT_WIN10), 0, Build_RS1);
 }
 
 VERSIONHELPERAPI
 IsWindows10RS2OrGreater()
 {
-    return IsWindowsVersionOrGreater( HIBYTE( _WIN32_WINNT_WIN10 ), LOBYTE( _WIN32_WINNT_WIN10 ), 0, Build_RS2 );
+    return IsWindowsVersionOrGreater(HIBYTE(_WIN32_WINNT_WIN10), LOBYTE(_WIN32_WINNT_WIN10), 0, Build_RS2);
 }
 
 VERSIONHELPERAPI
 IsWindows10RS3OrGreater()
 {
-    return IsWindowsVersionOrGreater( HIBYTE( _WIN32_WINNT_WIN10 ), LOBYTE( _WIN32_WINNT_WIN10 ), 0, Build_RS3 );
+    return IsWindowsVersionOrGreater(HIBYTE(_WIN32_WINNT_WIN10), LOBYTE(_WIN32_WINNT_WIN10), 0, Build_RS3);
 }
 
 VERSIONHELPERAPI
 IsWindows10RS4OrGreater()
 {
-    return IsWindowsVersionOrGreater( HIBYTE( _WIN32_WINNT_WIN10 ), LOBYTE( _WIN32_WINNT_WIN10 ), 0, Build_RS4 );
+    return IsWindowsVersionOrGreater(HIBYTE(_WIN32_WINNT_WIN10), LOBYTE(_WIN32_WINNT_WIN10), 0, Build_RS4);
 }
 
 VERSIONHELPERAPI
 IsWindows10RS5OrGreater()
 {
-    return IsWindowsVersionOrGreater( HIBYTE( _WIN32_WINNT_WIN10 ), LOBYTE( _WIN32_WINNT_WIN10 ), 0, Build_RS5 );
+    return IsWindowsVersionOrGreater(HIBYTE(_WIN32_WINNT_WIN10), LOBYTE(_WIN32_WINNT_WIN10), 0, Build_RS5);
 }
 
 VERSIONHELPERAPI
 IsWindows1019H1OrGreater()
 {
-    return IsWindowsVersionOrGreater( HIBYTE( _WIN32_WINNT_WIN10 ), LOBYTE( _WIN32_WINNT_WIN10 ), 0, Build_19H1 );
+    return IsWindowsVersionOrGreater(HIBYTE(_WIN32_WINNT_WIN10), LOBYTE(_WIN32_WINNT_WIN10), 0, Build_19H1);
 }
 
 VERSIONHELPERAPI
 IsWindows1019H2OrGreater()
 {
-    return IsWindowsVersionOrGreater( HIBYTE( _WIN32_WINNT_WIN10 ), LOBYTE( _WIN32_WINNT_WIN10 ), 0, Build_19H2 );
+    return IsWindowsVersionOrGreater(HIBYTE(_WIN32_WINNT_WIN10), LOBYTE(_WIN32_WINNT_WIN10), 0, Build_19H2);
 }
 
 VERSIONHELPERAPI
 IsWindows1020H1OrGreater()
 {
-    return IsWindowsVersionOrGreater( HIBYTE( _WIN32_WINNT_WIN10 ), LOBYTE( _WIN32_WINNT_WIN10 ), 0, Build_20H1 );
+    return IsWindowsVersionOrGreater(HIBYTE(_WIN32_WINNT_WIN10), LOBYTE(_WIN32_WINNT_WIN10), 0, Build_20H1);
+}
+
+VERSIONHELPERAPI
+IsWindows11BetaOrGreater()
+{
+    return IsWindowsVersionOrGreater(HIBYTE(_WIN32_WINNT_WIN10), LOBYTE(_WIN32_WINNT_WIN10), 0, Build_Win11Beta);
 }
 
 VERSIONHELPERAPI
 IsWindowsServer()
 {
-    OSVERSIONINFOEXW osvi = { sizeof( osvi ), 0, 0, 0, 0, { 0 }, 0, 0, 0, VER_NT_WORKSTATION };
-    DWORDLONG        const dwlConditionMask = VerSetConditionMask( 0, VER_PRODUCT_TYPE, VER_EQUAL );
+    OSVERSIONINFOEXW osvi = { sizeof(osvi), 0, 0, 0, 0, { 0 }, 0, 0, 0, VER_NT_WORKSTATION };
+    DWORDLONG        const dwlConditionMask = VerSetConditionMask(0, VER_PRODUCT_TYPE, VER_EQUAL);
 
-    return !VerifyVersionInfoW( &osvi, VER_PRODUCT_TYPE, dwlConditionMask );
+    return !VerifyVersionInfoW(&osvi, VER_PRODUCT_TYPE, dwlConditionMask);
 }
 
